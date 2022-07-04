75c39ca28724effdfd973b8094eedeb1e840290d
shuklasid19 <shuklasid19@gmail.com>
2022-07-04 18:29:07 +0530

    setup-done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="wine Quality ", 
+    author="shuklasid19", 
+    packages=find_packages(),
+    license="MIT"
+)
